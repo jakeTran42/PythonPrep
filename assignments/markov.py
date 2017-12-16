b5154dc938e3918ed9c2248229d18d944d9f87d9
Jake Tran <jake.tranehs@gmail.com>
2017-12-16 01:31:48 -0800

    markov started

--- assignments/markov.py
+++ assignments/markov.py
@@ -1,1 +1,23 @@
-import stochastic  
+import random
+from dictogram import Dictogram
+
+def totalWords(histogram):
+
+    ''' Total Word of Hist'''
+
+    count = 0
+    for key, value in histogram.items():
+        count += value
+        return count
+
+def wordProbability(histogram):
+    total = totalWords(histogram)
+    count = 0
+    totalValue = 0
+    newHist = {}
+    for key, value in histogram.items():
+        weightedValue = (value / total)
+        totalValue = weightedValue + count
+        newHist[key] = value
+        count += weightedValue
+    return newHist
