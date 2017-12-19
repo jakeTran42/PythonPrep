e3b257c635b18ad285370b6b699f0aed14376bb6
Jake Tran <jake.tranehs@gmail.com>
2017-12-19 01:34:18 -0800

    markov 0.1

--- assignments/markov.py
+++ assignments/markov.py
@@ -21,3 +21,24 @@ def wordProbability(histogram):
         newHist[key] = value
         count += weightedValue
     return newHist
+
+# def getRandomWord(histogram, totalWord):
+#
+#     ''' Get random word out of histogram '''
+#
+#     random_num = random.randint(1, totalWords)
+#     for key, value in histogram.items():
+#         if value < random_num:
+#             random_num -= value
+#         else:
+#             return key
+
+# def startingWord(dictionary):
+#
+#     ''' Pick a word to start sentence '''
+#
+#     words = []
+#     for key in dictionary:
+#         words.append(key)
+#     randIndex = random.randint(0, len(words))
+#     return word_list[random_index]
